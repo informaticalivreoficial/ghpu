534be5072dcd63bc69ec4ff0e83028e3e5011c8e
informaticalivreoficial <renatomontanari@ymail.com>
2025-04-05 22:06:46 -0300

    laravel update 10 to 9

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
          * Package Service Providers...
          */
         Weidner\Goutte\GoutteServiceProvider::class,
-        //Spatie\Permission\PermissionServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
